5662089f0ec3cdfaf2b176f7bdc283539e02bc66
Tuna <airrloww@gmail.com>
2024-08-04 23:04:53 +0200

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -2,7 +2,24 @@ provider "aws" {
   region = "eu-north-1"
 }
 
+# terraform {
+#   backend "s3" {
+#     bucket                  = "terraforming-aws-state" # create this manually in cloud console
+#     key                     = "infra/terraform.tfstate.d" # the state file/dir
+#     region = "eu-north-1"
+#     encrypt        	   = true
+#     dynamodb_table = "terraforming-aws-state" # create this manually in cloud console
+#   }
+# }
 
-module "iam-group-with-policies" {
-  source = "./modules/iam-group-with-policies"
-}
+# module "iam-group-with-policies" {
+#   source = "./modules/iam-group-with-policies"
+# }
+
+# module "iam-user" {
+#   source = "./modules/iam-user"
+# }
+
+# module "s3-bucket" {
+#   source = "./modules/s3-bucket"
+# }
